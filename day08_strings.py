db147f7f93dc326232b69583ca03c0ad360eb99f
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 22:09:04 +0800

    Create day08_strings.py

---
+++ day08_strings.py
@@ -0,0 +1,19 @@
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+favourite_food = input("What is your favourite food?")
+
+print(first_name.upper())
+print(last_name.lower())
+print(len(favourite_food))
+print(first_name + " " + last_name)
+
+                                                       OUTPUT
+
+What is your first name?Jhanel
+What is your last name?Cabahug
+What is your favourite food?sinigang
+
+JHANEL
+cabahug
+8
+Jhanel Cabahug
