489b4f9416fdfffc5900d746f8a384a9fc535197
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 21:53:35 +0800

    Create day02_variables.py

---
+++ day02_variables.py
@@ -0,0 +1,17 @@
+name = "Precious"
+age = "18"
+course = "BSIT"
+favourite_color = "Purple"
+
+print("My name is", name)
+print("I am", age)
+print("I am an", course)
+print("My favourite color is", favourite_color)
+
+
+                                                         OUTPUT
+
+My name is Precious
+I am 18
+I am an BSIT
+My favourite color is Purple
