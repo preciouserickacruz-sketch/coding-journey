2eb637ab38d15923dd4ea3045ebf51b93d881b29
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 21:50:22 +0800

    Create day01_for_loop.py

---
+++ day01_for_loop.py
@@ -0,0 +1,8 @@
+print("My name is Precious Ericka.")
+print("I am currently a 1st year BSIT student.")
+print("I want to learn coding because I want to improve my programming skills.")
+
+                                     OUTPUT
+  My name is Precious Ericka
+  I am currently a 1st year BSIT student
+  I want to learn coding because i want to improve my programming skills.
