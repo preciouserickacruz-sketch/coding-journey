f64ca3c85b4f325e2e0ae39fb97fcc6fb8d6bccc
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 22:01:00 +0800

    Create day05_if_else.py

---
+++ day05_if_else.py
@@ -0,0 +1,14 @@
+age = int(input("How old are you?"))
+
+if age >= 18:
+    print("You are 18 or older.")
+else:
+    print("You are under 18.")
+
+                                                            OUTPUT
+
+How old are you? 17
+You are under 18.
+
+How old are you? 20
+You are 18 or older.
