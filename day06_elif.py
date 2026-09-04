04f4c5b5516c92ecb2a825de6e1164121c779047
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 22:04:13 +0800

    Create day06_elif.py

---
+++ day06_elif.py
@@ -0,0 +1,25 @@
+score = int(input("Enter your score."))
+
+if score >= 95:
+    print("Excellent!")
+elif score >= 85:
+    print("Very Good!")
+elif score >= 77:
+    print("Good Job!")
+else:
+    print("You need more practice.")
+
+                                                                      OUTPUT
+
+Enter your score. 97
+Excellent!
+
+
+Enter your score. 90
+Very Good!
+
+Enter your score. 80
+Good Job!
+
+Enter your score. 67
+You need more practice.
