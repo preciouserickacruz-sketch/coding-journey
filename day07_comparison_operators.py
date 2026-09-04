6445b5fa3f26a72b054c6c19ba414689858aaf40
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 22:06:33 +0800

    Create day07_comparison_operators.py

---
+++ day07_comparison_operators.py
@@ -0,0 +1,14 @@
+age = int(input("How old are you?"))
+
+if age >= 13 and age <= 19:
+    print("You are between 13 and 19.")
+else:
+    print("You are not between 13 and 19.")
+
+                                                          OUTPUT
+
+How old are you? 17
+You are between 13 and 19.
+
+How old are you? 5
+You are not between 13 and 19.
