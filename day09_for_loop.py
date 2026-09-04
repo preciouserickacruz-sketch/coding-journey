403424569a634107f1a4864149b53fe22fc9e00b
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 22:11:20 +0800

    Create day09_for_loop.py

---
+++ day09_for_loop.py
@@ -0,0 +1,18 @@
+name = input("What is your name?")
+times = int(input("How many times should I say your name?"))
+
+for i in range(times):
+    print(i + 1, name)
+
+                                                            OUTPUT
+
+What is your name? Precious
+How many times should I say your name?7
+
+1 Precious
+2 Precious
+3 Precious
+4 Precious
+5 Precious
+6 Precious
+7 Precious
