13b967659d6e36df5440f4610388b1e633c59407
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 21:57:52 +0800

    Create day04_math.py

---
+++ day04_math.py
@@ -0,0 +1,24 @@
+age = int(input("How old are you?"))
+x = 897
+y = 5
+
+print(x + y)
+print(age / y)
+print(x / y)
+print(age + x)
+print(x - y)
+print(age - y)
+print(x * y)
+print(age * x)
+
+                                                                    OUTPUT
+
+How old are you?  17
+902
+3.4
+179.4
+914
+892
+12
+4485
+15249
