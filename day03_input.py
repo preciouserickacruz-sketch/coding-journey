3ffc5e6b24ebb2c67288a265dcef66896458c9d1
preciouserickacruz-sketch <preciouserickacruz@gmail.com>
2026-09-04 21:56:13 +0800

    Create day03_input.py

---
+++ day03_input.py
@@ -0,0 +1,21 @@
+name = input("What is your name?")
+age = input("How old are you?")
+course = input("What is your course?")
+favourite_color = input("What is your favourite color?")
+
+print("Your name is", name)
+print("You are", age, "years old")
+print("Your course is", course)
+print("Your favourite color is", favourite_color)
+
+                                                    OUTPUT
+
+What is your name? SUN
+How old are you? 17
+What is your course? BSIT
+What is your favourite color? BLUE
+
+Your name is SUN
+You are 17 years old
+Your course is BSIT
+Your favourite color is BLUE
